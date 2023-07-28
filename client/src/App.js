6e74219166da967faf06636699ec31e3933f157f
Ross Aitken <raaitken@gmail.com>
2023-07-28 01:49:53 +0100

    Initial styling

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import ListHeader from './components/ListHeader';
+import './index.css';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <ListHeader listName={'🏖️ Holiday tick list'} />
+    </div>
+  );
+}
+
+export default App;
